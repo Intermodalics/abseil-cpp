6b216472ce811be72a0ff31f877d2943ddde41bd
Johannes Meyer <johannes@intermodalics.eu>
2020-02-16 16:58:10 +0100

    Catkinize abseil-cpp by exposing abseil's CMake config directly to other catkin packages

---
+++ CMake/AbseilCatkin.cmake
@@ -0,0 +1,27 @@
+# Preset cmake variables to comply with ROS/catkin conventions and
+# install package.xml
+
+## Temporarily change the project name to 'abseil_cpp', the catkin package name
+cmake_minimum_required(VERSION 3.5)
+project(abseil_cpp)
+
+find_package(catkin REQUIRED)
+
+## Create positision-independent code and set default visibility to hidden.
+# This is important to avoid ODR violations if static abseil libraries are
+# linked into shared libraries or executables.
+set(CMAKE_POSITION_INDEPENDENT_CODE ON)
+set(CMAKE_CXX_VISIBILITY_PRESET hidden)
+
+## Do not install in lib/<arch>.
+# TODO: not required? Libraries are not installed in lib/x86_64-linux-gnu although
+# AbseilInstallDirs.cmake uses GNUInstallDirs.
+#unset(CMAKE_LIBRARY_ARCHITECTURE)
+
+## catkin package configuration
+catkin_package(
+  CFG_EXTRAS ${PROJECT_SOURCE_DIR}/CMake/abseil_cpp-catkin-extras.cmake
+)
+
+## Restore original project 'absl'
+project(absl CXX)
